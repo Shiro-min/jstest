4469fa6cf423f801a02134a3143b64e363dfe0e4
Shiro-min <ocean.sheep.727@gmail.com>
2021-01-03 18:47:05 +0900

    text p56

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 'use strict';
 
+document.getElementById('choice').textContent = new Date();
+console.log(document.getElementById('choice').textContent);
+
 document.write("<h3>JavaScriptのテスト</h3>");
